624663cf1fb5b94686d368389559e619c2be582e
Max Carvajal <john.m.carvajal.ctr@army.mil>
2024-01-17 14:28:44 -0600

    Wandering Monster and Rest Turn implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Button, Stack, Typography} from "@mui/material";
 
-function App() {
+const App = () => {
+
+  const [turnCount, setTurnCount] = useState<number>(0);
+
+  const die = (size: number): number => Math.floor(Math.random() * size) + 1
+  const d6 = (): number => {
+      const result = die(6)
+      console.log(result)
+      return result
+  }
+
+  const turnModulator = (mod: number) => turnCount !== 0 && turnCount % mod === 0
+
+  const isWanderingMonster = (): 'visible' | 'hidden' => turnModulator(2) && d6() <= 1 ? 'visible' : 'hidden'
+  const isRestTurn = (): 'visible' | 'hidden' => turnModulator(6) ? 'visible' : 'hidden'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Stack
+        alignItems='center'
+        gap={2}
+    >
+        <Typography variant='h3'>{`Turn Count: ${turnCount}`}</Typography>
+        <Typography variant='h3' visibility={isWanderingMonster}>Wandering Monster</Typography>
+        <Typography variant='h3' visibility={isRestTurn}>Rest Turn</Typography>
+        <Button
+            variant='contained'
+            onClick={() => setTurnCount(turnCount + 1)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+            Next Turn
+        </Button>
+    </Stack>
   );
-}
+};
 
 export default App;
